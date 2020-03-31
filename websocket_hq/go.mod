92a7c50a156dcc72de2c1b7e064b3db8452da3c7
issac <haiqun.fan@hiii-life.com>
2020-03-31 15:21:08 +0800

    [feat] go websocket demo

---
+++ websocket_hq/go.mod
@@ -0,0 +1,5 @@
+module golang_demo/websocket_hq
+
+go 1.14
+
+require github.com/gorilla/websocket v1.4.2
--- websocket_hq/main.go
+++ websocket_hq/main.go
@@ -1,21 +1,41 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
-type test1 interface {
-	save()
-}
+import (
+	"flag"
+	"log"
+	"net/http"
+)
 
-type person1 struct {
-	name string
-}
+var addr = flag.String("127.0.0.1", ":8999", "http service address")
 
-type person2 struct {
-	age int
-}
-
-func (p *person1)save()  {
-
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
 }
 
 func main() {
-
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		// 将信息丢到处理队列
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
